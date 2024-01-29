07ec8f32949e3436e3116b08261eb55edf28239c
BibleProg <45607940+BibleProg@users.noreply.github.com>
2024-01-29 11:03:10 +0100

    Update save_over_ftp.py

--- save_over_ftp.py
+++ save_over_ftp.py
@@ -5,7 +5,7 @@ import time
 
 def recursive_download(handler, destination, remote_dir):
     if not os.path.exists(destination):
-        print("created dir " + '"' + destination + '"')
+        print(f'create dir "{destination}"')
         os.makedirs(destination)
 
     tree = handler.mlsd(remote_dir) 
@@ -18,9 +18,9 @@ def recursive_download(handler, destination, remote_dir):
 
 def download(handler,file, destination, remote_dir):
     filename = file[0]
-    print('download "' + filename + '"')
-    with open(destination+"/"+filename, "wb") as localfile:
-        ftp.retrbinary('RETR ' + remote_dir+"/"+filename, localfile.write, 1024)
+    print(f'download "{filename}"')
+    with open(f"{destination}/{filename}", "wb") as localfile:
+        ftp.retrbinary(f'RETR {remote_dir}/{filename}', localfile.write, 1024)
 
 def isdir(el):
     if el[1]['type'] == 'dir':
@@ -29,22 +29,51 @@ def isdir(el):
         return False
 
 
+def get_conf(file):
+    global mysite    
+    global username  
+    global password  
+    global remote_dir
+    
+    print(f"Loading {file}")
+    config = dict()
+    with open(file, "r") as conffile:
+        params = conffile.read().strip().split("\n")
+        for el in params:
+            el    = el.split("#")[0].strip()
+            if el != '':
+                name  = el.split("=")[0].strip()
+                value = el.split("=")[1].strip()
+                config[name] = value
+
+    mysite     = config["mysite"]
+    print(f"[CONFIG] : mysite = {mysite}")
+    username   = config["username"]
+    print(f"[CONFIG] : username = {username}")
+    password   = config["password"]
+    print(f"[CONFIG] : password = {password}")
+    remote_dir = config["remote_dir"]
+    print(f"[CONFIG] : remote_dir = {remote_dir}")
+
+
+
 
 if __name__ == "__main__":
 
-    mysite     = "ftp.nitroserv.games"
-    username   = ""
-    password   = ""
-    remote_dir = "Palworld/Pal/Saved/SaveGames/"
-    local_dir  = str(round(time.time())) + "/Palworld/Pal/Saved/SaveGames/"
- 
+
+
+
     try:
+        get_conf("nitroserv.config")
+        local_dir  = "backups/" + str(round(time.time())) + remote_dir
+        
         print("connexion....")
         ftp = ftplib.FTP(mysite, username, password)
         print("connected !")
 
         recursive_download(ftp, local_dir,remote_dir)
+
     except Exception as e:
-        input(e)
+        input(f"[ERROR] : {e}")
     else:
         input("Press enter...")
